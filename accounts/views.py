2e90959429dc14038eaa992a4824ec0890bd8725
Parag Shah <adaptives@gmail.com>
2012-03-01 12:01:20 +0530

    fixed code to render default user images

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 import lernado.accounts.models as models
 import lernado.accounts.forms as forms
 import mimetypes
@@ -23,20 +25,24 @@ def edit_profile(request, user_id):
         user = User.objects.get(id=user_id)
         user_profile = models.UserProfile.objects.get(user=user)
         if request.method == 'GET':
-            pic_file_path = settings.__getattr__('MEDIA_ROOT') + user_profile.profile_picture.url
-            pic_file = open(pic_file_path, 'rb')
-            file_data = {'profile_picture': SimpleUploadedFile(user_profile.profile_picture.name, pic_file.read())}
+            if user_profile.profile_picture:
+                pic_file_path = settings.__getattr__('MEDIA_ROOT') + user_profile.profile_picture.url            
+                pic_file = open(pic_file_path, 'rb')
+                file_data = {'profile_picture': SimpleUploadedFile(user_profile.profile_picture.name, pic_file.read())}
+            else:
+                file_data = {'profile_picture': None}
+                
             profile_form = forms.ProfileForm({'location': user_profile.location}, file_data)
             ctx = {'profile_form': profile_form, 'user_profile':user_profile}
             return render_to_response('edit_profile.html', RequestContext(request, ctx))
         elif request.method == 'POST':
             profile_form = forms.ProfileForm(request.POST, request.FILES)
-            if profile_form.is_valid():
-                print 'valid... processing'            
+            if profile_form.is_valid():            
                 user_profile.location = request.POST['location']
-                user_profile.profile_picture = request.FILES['profile_picture']
+                if 'profile_picture' in request.FILES:
+                    user_profile.profile_picture = request.FILES['profile_picture']
                 user_profile.save()
-                return HttpResponse("Thank you your new profile is saved")
+                return HttpResponseRedirect(reverse('lernado.accounts.views.view_profile', kwargs={'user_id':user.id}))
             else:
                 raise Http404
         else:
@@ -53,7 +59,7 @@ def profile_pic(request, user_id):
         fpath = settings.__getattr__('MEDIA_ROOT') + user_profile.profile_picture.url
     else:
         fpath = settings.DEFAULT_PERSON_IMAGE 
-    
+
     content = open(fpath, "rb")
     # content can be a String or an iterable
     return HttpResponse(content, mimetype=mimetypes.guess_type(fpath))
