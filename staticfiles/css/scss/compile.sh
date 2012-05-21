aa2e96e2d3733ffb2c4ddad64322159dd62344b2
Parag Shah <adaptives@gmail.com>
2012-05-21 16:57:27 +0530

    added nesting to the scss files and using generated css files

---
+++ staticfiles/css/scss/compile.sh
@@ -0,0 +1,11 @@
+/home/pshah/workspace/sass/bin/scss ./container.scss > ../container.css
+/home/pshah/workspace/sass/bin/scss ./courses.scss > ../courses.css
+/home/pshah/workspace/sass/bin/scss ./default.scss > ../default.css
+/home/pshah/workspace/sass/bin/scss ./forum.scss > ../forum.css
+/home/pshah/workspace/sass/bin/scss ./header_footer.scss > ../header_footer.css
+/home/pshah/workspace/sass/bin/scss ./nav.scss > ../nav.css
+/home/pshah/workspace/sass/bin/scss ./pagedown.scss > ../pagedown.css
+/home/pshah/workspace/sass/bin/scss ./submenu.scss > ../submenu.css
+
+
+
