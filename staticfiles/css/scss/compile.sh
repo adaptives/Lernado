1c0dc05228cbd324a8c7a24d5311e740fe577c98
Parag Shah <adaptives@gmail.com>
2012-05-23 18:58:37 +0530

    made all individual scss files into partials, now we have only 1 css file created from all scss files, refactored forum css to change the width of a question count element which was earlier messing up the formatting

--- staticfiles/css/scss/compile.sh
+++ staticfiles/css/scss/compile.sh
@@ -1,11 +1,1 @@
-/home/pshah/workspace/sass/bin/scss ./container.scss > ../container.css
-/home/pshah/workspace/sass/bin/scss ./courses.scss > ../courses.css
-/home/pshah/workspace/sass/bin/scss ./default.scss > ../default.css
-/home/pshah/workspace/sass/bin/scss ./forum.scss > ../forum.css
-/home/pshah/workspace/sass/bin/scss ./header_footer.scss > ../header_footer.css
-/home/pshah/workspace/sass/bin/scss ./nav.scss > ../nav.css
-/home/pshah/workspace/sass/bin/scss ./pagedown.scss > ../pagedown.css
-/home/pshah/workspace/sass/bin/scss ./submenu.scss > ../submenu.css
-
-
-
+/home/pshah/workspace/sass/bin/scss ./main.scss > ../main.css
